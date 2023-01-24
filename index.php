bd876ce87128e44f6e41517eefb911e063105e9c
Dee Brecke <brecke.deirdre@student.greenriver.edu>
2023-01-24 18:46:17 -0500

    Changed and added routing

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+//start a session
+session_start();
+
 //Require autoload file
 require_once("vendor/autoload.php");
 
@@ -31,6 +34,19 @@ $f3->route('GET /lunch', function(){
     echo $view->render('views/lunch.html');
 });
 
+//order1 route -> views/order-form1.html
+$f3->route('GET|POST /order1', function(){
+//FI THE FORM HAS BEEN SUBMITTED
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+    $view = new Template();
+    echo $view->render('views/order-form1.html');
+});
+
+$f3->route('GET /summary', function(){
+    //echo '<h1>My Diner</h1>';
+    $view = new Template();
+    echo $view->render('views/order-summary.html');
+});
 //Run fat free
 $f3->run();
 
